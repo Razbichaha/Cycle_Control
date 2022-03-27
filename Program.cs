70eadda85b27180ebb2304a30580a61fa23acab8
Сергей <seregabur@live.ru>
2022-03-27 20:07:36 +0300

    Добавьте файлы проекта.

---
+++ Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace Cycle_Control
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+           
+            string cycleShutdownWord = "exit";
+            bool flag = true;
+
+            while(flag)
+            {
+                 Console.WriteLine("Для останова программы наберите exit");
+                string temp = Console.ReadLine();
+                if(temp.Equals(cycleShutdownWord))
+                {
+                    flag = false;
+                    Console.WriteLine("Подтвержден выход из цикла");
+                }
+            }
+        }
+    }
+}
